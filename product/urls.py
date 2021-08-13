1f3cfecfcaacf7d69b233b5b1252f832495e5104
kackey0-1 <kentaro.a.kakimoto@gmail.com>
2021-08-13 14:18:05 +0900

    add deploy action for ecr.

--- product/urls.py
+++ product/urls.py
@@ -3,8 +3,8 @@ from .views import ProductIndexView, ProductCreateView, ProductDetailView, Produ
 
 app_name = 'product'
 urlpatterns = [
-    path('products', ProductIndexView.as_view(), name='index'),
-    path('product', ProductCreateView.as_view(), name='product'),
+    path('products/', ProductIndexView.as_view(), name='index'),
+    path('product/', ProductCreateView.as_view(), name='product'),
     path('product/<int:pk>', ProductDetailView.as_view(), name='detail'),
     path('product/<int:pk>/edit', ProductEditView.as_view(), name='edit'),
     path('product/<int:pk>/delete', ProductDeleteView.as_view(), name='delete'),
