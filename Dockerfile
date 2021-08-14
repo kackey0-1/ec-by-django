09ac2749df17ec8e8a75a38b0b3bd65efbe6cb45
kackey0-1 <kentaro.a.kakimoto@gmail.com>
2021-08-14 19:28:13 +0900

    add iam role for task v6

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM python:3
 
+USER ec-service
+
 WORKDIR /usr/src/app
 
 COPY . .
--- aws-resources/modules/vpc/vpc.tf
+++ aws-resources/modules/vpc/vpc.tf
@@ -7,11 +7,12 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "2.70.0"
 
-  name           = "vpc-django-dev"
-  cidr           = "10.0.0.0/16"
-  azs            = data.aws_availability_zones.available.names
-  public_subnets = ["10.0.1.0/24", "10.0.3.0/24", "10.0.5.0/24"]
-  private_subnets = ["10.0.2.0/24", "10.0.4.0/24", "10.0.6.0/24"]
+  name               = "vpc-django-dev"
+  cidr               = "10.0.0.0/16"
+  azs                = data.aws_availability_zones.available.names
+  enable_nat_gateway = true
+  public_subnets     = ["10.0.1.0/24", "10.0.3.0/24", "10.0.5.0/24"]
+  private_subnets    = ["10.0.2.0/24", "10.0.4.0/24", "10.0.6.0/24"]
 
   tags = merge(
     var.DEFAULT_TAGS,
