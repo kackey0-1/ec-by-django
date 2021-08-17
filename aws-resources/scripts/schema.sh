ea48b07be93cb19433840030bfca1f0d567372bb
kackey0-1 <kentaro.a.kakimoto@gmail.com>
2021-08-17 18:24:16 +0900

    fix ci v8

--- aws-resources/scripts/schema.sh
+++ aws-resources/scripts/schema.sh
@@ -6,7 +6,7 @@ then
 fi
 
 mysql --host=${1} --port=${2} --user=${3} --password=${4}  << EOF
-CREATE DATABASE ${5};
+CREATE DATABASE ${5} CHARACTER SET UTF8mb4;
 CREATE USER '${5}' IDENTIFIED BY '${6}';
 GRANT ALL PRIVILEGES ON ${5}.* TO '${5}';
 EOF
