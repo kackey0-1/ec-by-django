67c351af90aae2673916b0db0111a01a80d7a1b4
kackey0-1 <kentaro.a.kakimoto@gmail.com>
2021-08-18 12:25:14 +0900

    fix ci v14

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,6 +5,6 @@ pip3 install -r requirements.txt
 # Set environment for production
 export DJANGO_PROFILE=production
 # Prepare database
-python3 manage.py migrate
+# python3 manage.py migrate
 # Start runserver
 gunicorn app.wsgi --reload --bind 0.0.0.0:8000
